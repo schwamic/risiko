3db3afd13edf4e3ebbfa93fde9ceb9c54735d8a8
schwamic <schwarz.michael@posteo.net>
2021-12-11 16:48:34 +0100

    refactor(client): list of players

--- packages/client/src/containers/game-page/players-list/players-list.jsx
+++ packages/client/src/containers/game-page/players-list/players-list.jsx
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { isNil } from 'lodash'
 import { Grid, Typography } from '@mui/material'
 import { ReactComponent as GhostImage } from '../../../lib/assets/ghost_1079732.svg'
 import enums from '../../../lib/common/enums'
@@ -7,22 +8,30 @@ import { styles } from './players-list.styles'
 import { content } from './players-list.content'
 
 function PlayersList ({ players, player, ...props }) {
-  const parsePlayers = (players, state) => {
+  const getPlayersWithoutMyself = () => {
     const playerId = player?.playerId
-    return players.filter(player => player.state === state && player.playerId !== playerId)
+    if (!isNil(playerId) && !isNil(players)) {
+      return players.filter(player => player.playerId !== playerId)
+    } else {
+      return players
+    }
+  }
+
+  const parsePlayers = (state) => {
+    return getPlayersWithoutMyself().filter(player => player.state === state)
   }
 
   return (
     <Grid container item spacing={5} justifyContent={{ xs: 'center', sm: 'start' }}>
-      {players?.length > 1
+      {getPlayersWithoutMyself()?.length > 0
         ? (
           <>
-            {players.length > 0 && parsePlayers(players, enums.playerStates.offline).map((player) => (
+            {players.length > 0 && parsePlayers(enums.playerStates.offline).map((player) => (
               <Grid item key={player.playerId}>
                 <Avatar player={player} fontVariant='body3' />
               </Grid>
             ))}
-            {players.length > 0 && parsePlayers(players, enums.playerStates.online).map((player) => (
+            {players.length > 0 && parsePlayers(enums.playerStates.online).map((player) => (
               <Grid item key={player.playerId}>
                 <Avatar player={player} fontVariant='body3' />
               </Grid>
