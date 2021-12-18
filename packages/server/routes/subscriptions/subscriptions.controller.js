7680b2f564e486477058032b9b05807bb66a962d
schwamic <schwarz.michael@posteo.net>
2021-12-18 13:52:25 +0100

    chore: cleaning up

--- packages/server/routes/subscriptions/subscriptions.controller.js
+++ packages/server/routes/subscriptions/subscriptions.controller.js
@@ -1,6 +1,19 @@
 const { QueryService } = require('@lib/infrastructure/database')
 const enums = require('@lib/common/enums')
 
+/**
+ * Broadcasts an event to all clients.
+ * To have something like "rooms" you have to handle the logic on our own.
+ * E.g. create a "rooms" table in the database to save and sort all active clients.
+ */
+const _broadcast = (fastify, request, type) => {
+  fastify.websocketServer.clients.forEach(function (client) {
+    if (client.readyState === 1) {
+      client.send(JSON.stringify({ ...request.query, type }))
+    }
+  })
+}
+
 module.exports = function (fastify) {
   return {
     global: function (connection, request) {
@@ -8,11 +21,7 @@ module.exports = function (fastify) {
        * Case: ONLINE
        */
       connection.socket.on('message', function () {
-        fastify.websocketServer.clients.forEach(function (client) {
-          if (client.readyState === 1) {
-            client.send(JSON.stringify({ ...request.query, type: 'message' }))
-          }
-        })
+        _broadcast(fastify, request, 'message')
       })
 
       /**
@@ -22,11 +31,7 @@ module.exports = function (fastify) {
         const queryService = new QueryService()
         const client = await fastify.pg.connect()
         await client.query(queryService.players.updateState, [enums.playerStates.offline, request.query.playerId, request.query.gameId])
-        fastify.websocketServer.clients.forEach(function (client) {
-          if (client.readyState === 1) {
-            client.send(JSON.stringify({ ...request.query, type: 'close' }))
-          }
-        })
+        _broadcast(fastify, request, 'close')
         client.release()
       })
     }
